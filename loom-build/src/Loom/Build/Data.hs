c1ef3a3717fc21b818ab72157bce731db31154b6
Charles O'Farrell <charleso@charleso.org>
2017-01-24 16:44:40 +1100

    Introduce basic loom configuration

---
+++ loom-build/src/Loom/Build/Data.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Loom.Build.Data (
+    FilePattern (..)
+  , LoomConfig (..)
+  , LoomConfigResolved (..)
+  , compileFilePattern
+  , renderFilePattern
+  , appendFilePattern
+  ) where
+
+import qualified Data.Text as T
+
+import           P
+
+import           System.FilePath (FilePath)
+import qualified System.FilePath.Glob as G
+
+newtype FilePattern =
+  FilePattern G.Pattern
+    deriving (Eq, Show)
+
+data LoomConfig =
+  LoomConfig {
+      loomConfigComponents :: [FilePattern]
+    , loomConfigSass :: [FilePattern]
+    } deriving (Eq, Show)
+
+data LoomConfigResolved =
+  LoomConfigResolved {
+      loomConfigResolvedComponents :: [FilePath]
+    , loomConfigResolvedSass :: [FilePath]
+    } deriving (Eq, Show)
+
+compileFilePattern :: Text -> Either Text FilePattern
+compileFilePattern =
+  let
+    options =
+      G.CompOptions {
+          G.characterClasses = False
+        , G.characterRanges = False
+        , G.numberRanges = False
+        , G.wildcards = True
+        , G.recursiveWildcards = False
+        , G.pathSepInRanges = False
+        , G.errorRecovery = False
+        }
+  in
+    bimap T.pack FilePattern . G.tryCompileWith options . T.unpack
+
+renderFilePattern :: FilePattern -> Text
+renderFilePattern (FilePattern t) =
+  T.pack . G.decompile $ t
+
+appendFilePattern :: FilePattern -> FilePattern -> Either Text FilePattern
+appendFilePattern f1 f2 =
+  compileFilePattern $
+    renderFilePattern f1 <> "/" <> renderFilePattern f2
---
+++ loom-build/test/Test/Loom/Build/Gen/Data.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Test.Loom.Build.Gen.Data (
+    genFilePattern
+  ) where
+
+import           Loom.Build.Data
+
+import           P
+
+import           Test.QuickCheck.Jack (Gen)
+import qualified Test.QuickCheck.Jack as J
+
+genFilePattern :: Gen FilePattern
+genFilePattern =
+  J.oneOfRec
+    [genFilePatternSegment]
+    [J.justOf . fmap rightToMaybe $ appendFilePattern <$> genFilePatternSegment <*> genFilePattern]
+
+genFilePatternSegment :: Gen FilePattern
+genFilePatternSegment =
+  J.justOf $
+    rightToMaybe . compileFilePattern
+      <$> J.elements ["module", "component", "src", "app", "*"]
